20e71804ceebbebdc415675b36580149425db81e
Яков Савельев <jakobs@Akovs-MacBook-Pro.local>
2025-12-03 22:37:15 +0100

    join-interlocutor

--- schat-app/src/chat/gateways/chat.gateway.ts
+++ schat-app/src/chat/gateways/chat.gateway.ts
@@ -1,15 +1,17 @@
-import { UseFilters } from '@nestjs/common';
 import {
   WebSocketGateway,
   SubscribeMessage,
-  MessageBody,
-  ConnectedSocket,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { WsErrorFilter } from 'shared/ws-error.filter';
+import { ChatService } from '../services/chat.service';
+import { Namespace, Socket } from 'socket.io';
+import { UseFilters } from '@nestjs/common';
+import { socketMessageNamespaces } from '../constants/chat.constants';
+import { MessageService } from '../services/message.service';
+import { PostRoomMessageDto } from '../dto/room-message.dto';
+import { WsErrorFilter } from 'src/shared/ws-error.filter';
 
 @WebSocketGateway({ namespace: 'chat_room' })
-// @UseGuards(AuthGuard('jwt'))
 @UseFilters(new WsErrorFilter())
 export class ChatGateway {
   constructor(
@@ -17,5 +19,57 @@ export class ChatGateway {
     private readonly messageService: MessageService,
   ) {
     // startMonitoringActiveUsers(3000, this.activeUsers);
+  }
+
+  @WebSocketServer()
+  io: Namespace; // server: Server not working. Use "Namespace" instead;
+  private activeUsers = new Map<string, Set<string>>();
+  async handleConnection(client: Socket) {
+    const { userId, roomIds } = client.handshake.query as {
+      userId: string;
+      roomIds?: string;
+    };
+    if (!userId) {
+      console.error('Invalid connection query params');
+      return;
+    }
+
+    this.chatService.addInterlocutorToActiveUsers({
+      clientId: client.id,
+      userId,
+      activeUsers: this.activeUsers,
+    });
+
+    /* Join user to the chatroom only if he's entered the chat room on the frontend */
+    if (roomIds) {
+      this.chatService.handleJoinSingleUserChatRoom({
+        roomId: roomIds,
+        client,
+        userId,
+        activeUsers: this.activeUsers,
+        io: this.io,
+      });
+    }
+  }
+
+  async handleDisconnect(client: Socket) {
+    const userId = client.handshake.query.userId as string;
+
+    this.chatService.roomsDisconnectInterlocutor({
+      clientId: client.id,
+      userId,
+      activeUsers: this.activeUsers,
+      io: this.io,
+    });
+  }
+
+  @SubscribeMessage(socketMessageNamespaces.CHAT_ROOM_MESSAGE)
+  handlePostRoomMessage(client: Socket, dto: PostRoomMessageDto) {
+    this.messageService.postRoomMessage({
+      dto,
+      client,
+      activeUsers: this.activeUsers,
+      io: this.io,
+    });
   }
 }
--- schat-app/src/chat/services/chat.service.ts
+++ schat-app/src/chat/services/chat.service.ts
@@ -3,6 +3,7 @@ import { MOCKED_CHAT_ROOMS } from '../constants/mocked';
 import { ChatRoom } from '../schemas/chat-room.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Namespace, Socket } from 'socket.io';
 
 @Injectable()
 export class ChatService implements OnModuleInit {
@@ -26,5 +27,152 @@ export class ChatService implements OnModuleInit {
 
   async onModuleInit() {
     this.initializeDefaultChatRooms();
+  }
+
+  async addInterlocutorToActiveUsers({
+    clientId,
+    userId,
+    activeUsers,
+  }: {
+    clientId: string;
+    userId: string;
+    activeUsers: Map<string, Set<string>>;
+  }) {
+    if (!activeUsers.has(userId)) {
+      activeUsers.set(userId, new Set());
+    }
+
+    if (activeUsers.get(userId).has(clientId)) {
+      return;
+    }
+
+    activeUsers.get(userId).add(clientId);
+  }
+
+  handleJoinSingleUserChatRoom = async ({
+    client,
+    userId,
+    roomId,
+    activeUsers,
+    io,
+  }: {
+    client: Socket;
+    userId: string;
+    roomId: string;
+    activeUsers: Map<string, Set<string>>;
+    io: Namespace;
+  }) => {
+    try {
+      const chatRoom = await this.chatRoomRepository.findOne({
+        where: { id: roomId },
+        relations: ['participants'],
+      });
+
+      const isInterlocutorRelatedToChatRoom = chatRoom.participants.find(
+        (participant) => participant.interlocutor_id === userId,
+      );
+
+      if (!isInterlocutorRelatedToChatRoom) {
+        const errorMessage = strings.userNotInvited;
+        throw new Error(errorMessage);
+      }
+
+      const roomParticipiantsPublicKeys = chatRoom.participants.map(
+        (participant) => {
+          return participant.public_chat_key;
+        },
+      );
+
+      await this.handleJoinChat({
+        client,
+        dto: { userChatIds: [roomId], interlocutorId: userId },
+        activeUsers,
+      });
+
+      io.to(roomId).emit(chatRoomEmitEvents.USER_JOINED_CHAT, {
+        message: strings.privateRoomCreated,
+      });
+
+      client.emit(chatRoomEmitEvents.JOIN_CHAT_SUCCESS, {
+        message: strings.joinChatSuccess.replace(
+          '${chatName}',
+          chatRoom.chat_name,
+        ),
+        data: roomParticipiantsPublicKeys,
+      });
+    } catch (error) {
+      client.emit(chatRoomEmitEvents.JOIN_CHAT_ERROR, {
+        message: error.message,
+      });
+    }
+  };
+
+  async roomsDisconnectInterlocutor({
+    clientId,
+    userId,
+    activeUsers,
+    io,
+  }: {
+    clientId: string;
+    userId: string;
+    activeUsers: Map<string, Set<string>>;
+    io: Namespace;
+  }) {
+    try {
+      if (activeUsers.has(userId)) {
+        activeUsers.get(userId).delete(clientId);
+
+        if (activeUsers.get(userId).size === 0) {
+          activeUsers.delete(userId);
+          console.log(`User ${userId} is no longer active.`);
+        }
+      }
+
+      const roomsWithCurrentUser = Array.from(activeUsers.entries()).filter(
+        ([, sockets]) => sockets.has(userId),
+      );
+
+      for (const [roomId, sockets] of roomsWithCurrentUser) {
+        sockets.forEach((socketId) => {
+          io.to(socketId).emit(chatRoomEmitEvents.USER_LEFT_CHAT, {
+            userId,
+            room: roomId,
+          });
+        });
+      }
+    } catch (error) {
+      console.error(strings.userDisconnectingError, error);
+    }
+  }
+
+  async handleJoinChat({
+    client,
+    dto,
+    activeUsers,
+  }: {
+    client: Socket;
+    dto: { userId: string };
+    activeUsers: Map<string, Set<string>>;
+  }) {
+    const { userId } = dto;
+
+    try {
+      userChatIds.forEach((chatId) => {
+        if (!activeUsers.has(chatId)) {
+          activeUsers.set(userId, new Set());
+        }
+
+        /* Adding interlocutor to the chat */
+        activeUsers.get(chatId).add(interlocutorId);
+      });
+
+      /* Adding (activating) Room Id in the socket client flow */
+      client.join(userChatIds);
+    } catch (error) {
+      console.error(strings.joinChatError, error);
+      client.emit(chatRoomEmitEvents.JOIN_CHAT_ERROR, {
+        message: error.message,
+      });
+    }
   }
 }
